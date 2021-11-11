b2333885e1ce1ffddf6247b3b99287d998c6c2fe
jlandowner <jlandowner8@gmail.com>
2021-11-11 14:08:38 +0000

    update cosmoctl flags

--- cmd/cosmoctl/main.go
+++ cmd/cosmoctl/main.go
@@ -39,7 +39,7 @@ MIT 2021 cosmo-workspace/cosmo
 
 	rootCmd.PersistentFlags().StringVar(&o.KubeConfigPath, "kubeconfig", "", "kubeconfig file path (default: $HOME/.kube/config)")
 	rootCmd.PersistentFlags().StringVar(&o.KubeContext, "context", "", "kube-context (default: current context)")
-	rootCmd.PersistentFlags().IntVar(&o.LogLevel, "v", -1, "log level. -1:DISABLED, 0:INFO, 1:DEBUG, 2:ALL")
+	rootCmd.PersistentFlags().IntVarP(&o.LogLevel, "verbose", "v", -1, "log level. -1:DISABLED, 0:INFO, 1:DEBUG, 2:ALL")
 
 	template.AddCommand(rootCmd, o)
 	user.AddCommand(rootCmd, o)
