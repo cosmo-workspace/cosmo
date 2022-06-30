abd0c0103f0b6453f5b496558248bbbdb4b3da24
oruharo <9918931+oruharo@users.noreply.github.com>
2022-07-01 05:33:06 +0900

    Add @emotion/jest & update UI test snapshot files

--- web/dashboard-ui/src/setupTests.ts
+++ web/dashboard-ui/src/setupTests.ts
@@ -3,15 +3,18 @@
 // expect(element).toHaveTextContent(/react/i)
 // learn more: https://github.com/testing-library/jest-dom
 import '@testing-library/jest-dom';
+import { createSerializer } from '@emotion/jest';
 
 // https://github.com/mui-org/material-ui/issues/21701
-expect.addSnapshotSerializer({
-	test: function (val) {
-		return val && typeof val === "string" && val.indexOf("mui-") >= 0;
-	},
-	print: function (val) {
-		let str = val as string;
-		str = str.replace(/mui-[0-9]*/g, "mui-0000_for_toMatchSnapShot");
-		return `"${str}"`;
-	}
-});
+// expect.addSnapshotSerializer({
+// 	test: function (val) {
+// 		return val && typeof val === "string" && val.indexOf("mui-") >= 0;
+// 	},
+// 	print: function (val) {
+// 		let str = val as string;
+// 		str = str.replace(/mui-[0-9]*/g, "mui-0000_for_toMatchSnapShot");
+// 		return `"${str}"`;
+// 	}
+// });
+
+expect.addSnapshotSerializer(createSerializer())
