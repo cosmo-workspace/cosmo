01ff1ee0b9f0bf2c4aadce25f402674960a9cd4d
oruharo <9918931+oruharo@users.noreply.github.com>
2022-07-29 06:45:10 +0900

    change command name and flag name

--- internal/cmd/workspace/add_net_rule.go
+++ internal/cmd/workspace/add_net_rule.go
@@ -14,11 +14,11 @@ import (
 	"github.com/cosmo-workspace/cosmo/pkg/cmdutil"
 )
 
-type openPortOption struct {
+type addNetRuleOption struct {
 	*cmdutil.UserNamespacedCliOptions
 
 	WorkspaceName string
-	PortName      string
+	NetRuleName   string
 	PortNumber    int
 	Group         string
 	HTTPPath      string
@@ -27,25 +27,25 @@ type openPortOption struct {
 	rule wsv1alpha1.NetworkRule
 }
 
-func openPortCmd(cliOpt *cmdutil.UserNamespacedCliOptions) *cobra.Command {
-	o := &openPortOption{UserNamespacedCliOptions: cliOpt}
+func addNetRuleCmd(cliOpt *cmdutil.UserNamespacedCliOptions) *cobra.Command {
+	o := &addNetRuleOption{UserNamespacedCliOptions: cliOpt}
 
 	cmd := &cobra.Command{
-		Use:               "open-port WORKSPACE_NAME --name PORT_NAME --port PORT_NUMBER",
-		Short:             "Update or insert workspace network port",
+		Use:               "add-net-rule WORKSPACE_NAME --name NETWORK_RULE_NAME --port PORT_NUMBER",
+		Short:             "Update or insert workspace network rule",
 		PersistentPreRunE: o.PreRunE,
 		RunE:              cmdutil.RunEHandler(o.RunE),
 	}
-	cmd.Flags().StringVar(&o.PortName, "name", "", "Serivce port name (Required)")
-	cmd.Flags().IntVar(&o.PortNumber, "port", 0, "Serivce port number (Required)")
-	cmd.Flags().StringVar(&o.Group, "group", "", "Group of ports for URLVar. Ports in the same group are treated as the same domain. set port-name value if empty")
-	cmd.Flags().StringVar(&o.HTTPPath, "path", "/", "Path for Ingress path when using ingress")
+	cmd.Flags().StringVar(&o.NetRuleName, "name", "", "network rule name (Required)")
+	cmd.Flags().IntVar(&o.PortNumber, "port", 0, "serivce port number (Required)")
+	cmd.Flags().StringVar(&o.Group, "group", "", "group of ports for URLVar. Ports in the same group are treated as the same domain. set 'name' value if empty")
+	cmd.Flags().StringVar(&o.HTTPPath, "path", "/", "path for Ingress path when using ingress")
 	cmd.Flags().BoolVar(&o.Public, "public", false, "disable authentication for this port")
 
 	return cmd
 }
 
-func (o *openPortOption) PreRunE(cmd *cobra.Command, args []string) error {
+func (o *addNetRuleOption) PreRunE(cmd *cobra.Command, args []string) error {
 	if err := o.Validate(cmd, args); err != nil {
 		return fmt.Errorf("validation error: %w", err)
 	}
@@ -55,7 +55,7 @@ func (o *openPortOption) PreRunE(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func (o *openPortOption) Validate(cmd *cobra.Command, args []string) error {
+func (o *addNetRuleOption) Validate(cmd *cobra.Command, args []string) error {
 	if o.AllNamespace {
 		return errors.New("--all-namespaces is not supported in this command")
 	}
@@ -65,27 +65,27 @@ func (o *openPortOption) Validate(cmd *cobra.Command, args []string) error {
 	if len(args) < 1 {
 		return errors.New("invalid args")
 	}
-	if o.PortName == "" {
-		return errors.New("--port-name is required")
+	if o.NetRuleName == "" {
+		return errors.New("--name is required")
 	}
 	if o.PortNumber == 0 {
-		return errors.New("--port-number is required")
+		return errors.New("--port is required")
 	}
 	return nil
 }
 
-func (o *openPortOption) Complete(cmd *cobra.Command, args []string) error {
+func (o *addNetRuleOption) Complete(cmd *cobra.Command, args []string) error {
 	if err := o.UserNamespacedCliOptions.Complete(cmd, args); err != nil {
 		return err
 	}
 	o.WorkspaceName = args[0]
 
 	if o.Group == "" {
-		o.Group = o.PortName
+		o.Group = o.NetRuleName
 	}
 
 	o.rule = wsv1alpha1.NetworkRule{
-		PortName:   o.PortName,
+		PortName:   o.NetRuleName,
 		PortNumber: o.PortNumber,
 		HTTPPath:   o.HTTPPath,
 		Group:      pointer.String(o.Group),
@@ -94,7 +94,7 @@ func (o *openPortOption) Complete(cmd *cobra.Command, args []string) error {
 	return nil
 }
 
-func (o *openPortOption) RunE(cmd *cobra.Command, args []string) error {
+func (o *addNetRuleOption) RunE(cmd *cobra.Command, args []string) error {
 	ctx, cancel := context.WithTimeout(o.Ctx, time.Second*10)
 	defer cancel()
 	ctx = clog.IntoContext(ctx, o.Logr)
@@ -106,6 +106,6 @@ func (o *openPortOption) RunE(cmd *cobra.Command, args []string) error {
 		return err
 	}
 
-	cmdutil.PrintfColorInfo(o.Out, "Successfully open port '%s' for workspace '%s'\n", o.PortName, o.WorkspaceName)
+	cmdutil.PrintfColorInfo(o.Out, "Successfully add network rule '%s' for workspace '%s'\n", o.NetRuleName, o.WorkspaceName)
 	return nil
 }
