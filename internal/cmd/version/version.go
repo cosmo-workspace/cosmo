1c8d26cfa469b7b7311a20c640ba7ca1f30f1d2e
jlandowner <jlandowner@users.noreply.github.com>
2023-07-22 02:10:39 +0000

    Bump version: v1.0.0-rc1

--- internal/cmd/version/version.go
+++ internal/cmd/version/version.go
@@ -7,7 +7,7 @@ import (
 	"github.com/spf13/cobra"
 )
 
-const Footprint = `cosmoctl - cosmo v0.11.0 cosmo-workspace 2023`
+const Footprint = `cosmoctl - cosmo v1.0.0-rc1 cosmo-workspace 2023`
 
 func AddCommand(cmd *cobra.Command, o *cmdutil.CliOptions) {
 	versionCmd := &cobra.Command{
